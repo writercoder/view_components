344f50bef790490d2ccbdb2dbff20de26d5f0725
Cameron Dutro <camertron@gmail.com>
2022-10-13 20:34:11 +0000

    Add preview showing trailing action

--- previews/primer/beta/flash_preview.rb
+++ previews/primer/beta/flash_preview.rb
@@ -15,6 +15,18 @@ module Primer
       def default(full: false, spacious: false, dismissible: false, icon: :people, scheme: Primer::Beta::Flash::DEFAULT_SCHEME, content: "This is a flash message!")
         render(Primer::Beta::Flash.new(full: full, spacious: spacious, dismissible: dismissible, icon: icon == :none ? nil : icon, scheme: scheme)) { content }
       end
+
+      # @label With action
+      #
+      # @param full toggle
+      # @param spacious toggle
+      # @param dismissible toggle
+      # @param icon [Symbol] select [alert, check, info, people]
+      # @param scheme [Symbol] select [default, warning, danger, success]
+      # @param content text
+      def with_action(full: false, spacious: false, dismissible: false, icon: :people, scheme: Primer::Beta::Flash::DEFAULT_SCHEME, content: "This is a flash message with an action!")
+        render_with_template(locals: { full: full, spacious: spacious, dismissible: dismissible, icon: icon, scheme: scheme, content: content })
+      end
     end
   end
 end
