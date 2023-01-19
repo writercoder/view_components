01d2167dc100545672e8dbeb20d35eab401a5960
Joel Hawksley <joel@hawksley.org>
2023-01-19 15:51:49 -0700

    works

---
+++ app/components/primer/primer.js
@@ -0,0 +1,16 @@
+import './alpha/dropdown';
+import './alpha/image_crop';
+import './alpha/modal_dialog';
+import './alpha/nav_list';
+import './alpha/segmented_control';
+import './alpha/toggle_switch';
+import './alpha/tool_tip';
+import './alpha/x_banner';
+import './beta/auto_complete/auto_complete';
+import './beta/clipboard_copy';
+import './local_time';
+import './alpha/tab_container';
+import './time_ago_component';
+import '../../../lib/primer/forms/primer_multi_input';
+import '../../../lib/primer/forms/primer_text_field';
+import '../../../lib/primer/forms/toggle_switch_input';
--- test/accessibility_test.rb
+++ test/accessibility_test.rb
@@ -3,29 +3,8 @@
 require "system/test_case"
 
 class AccessibilityTest < System::TestCase
-  parallelize workers: 4
-
-  # Skip components that should be tested as part of a larger component.
-  # Do not add to this list for any other reason!
-  IGNORED_PREVIEWS = %w[
-    Primer::Beta::MarkdownPreview
-    Primer::Beta::AutoCompleteItemPreview
-    Docs::BetaAutoCompleteItemPreview
-    Docs::NavigationTabComponentPreview
-  ].freeze
-
-  ViewComponent::Preview.all.each do |klass|
-    next if IGNORED_PREVIEWS.include?(klass.to_s)
-
-    component_previews = klass.instance_methods(false)
-    component_uri = klass.to_s.underscore.gsub("_preview", "")
-
-    component_previews.each do |preview|
-      define_method(:"test_#{component_uri.parameterize(separator: "_")}_#{preview}") do
-        visit("/rails/view_components/#{component_uri}/#{preview}")
-        assert_accessible
-        puts "#{component_uri}##{preview} passed check."
-      end
-    end
+  def test_google
+    visit("https://www.hawksley.org:80")
+    assert_accessible
   end
 end
