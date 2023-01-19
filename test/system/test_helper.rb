c3aa3bba07af3db93d03b7e37259691d69c6a57a
Joel Hawksley <joel@hawksley.org>
2023-01-19 15:57:28 -0700

    still works

--- test/system/test_helper.rb
+++ test/system/test_helper.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require "webmock/minitest"
-
-WebMock.disable_net_connect!(allow_localhost: true)
-
-ENV["TZ"] = "Asia/Taipei"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,26 +2,7 @@
 
 ENV["RAILS_ENV"] = "test"
 
-if ENV["COVERAGE"] == "1"
-  require "simplecov"
-
-  if ENV["CI"] == "true"
-    SimpleCov.formatter SimpleCov::Formatter::SimpleFormatter
-  else
-    require "simplecov-console"
-    SimpleCov::Formatter::Console.output_style = "table"
-  end
-end
-
 require "minitest/autorun"
-require "mocha/minitest"
 require "rails"
-require "rails/test_help"
-require "view_component/test_helpers"
-require "test_helpers/component_test_helpers"
-require "action_controller/railtie"
-require "rails/test_unit/railtie"
-require "active_model/railtie"
-require "primer/view_components"
 
 require File.expand_path("../demo/config/environment.rb", __dir__)
